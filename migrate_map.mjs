e900e97394c77124dd2654b566c250d9a2e5ace1
Meow <me@itsmeow.cat>
2025-03-10 22:41:52 -0400

    basic script

---
+++ migrate_map.mjs
@@ -0,0 +1,50 @@
+import fs from "fs";
+
+const Map = {
+  _id: null,
+  name: "test",
+  mode: "DM",
+  author: "test",
+  version: "1",
+  editorVersion: "alpha",
+  date: "2025/3/10",
+  contextData: {
+    environment: {},
+    go: [],
+  },
+};
+
+const json = JSON.parse(fs.readFileSync("dump.json").toString());
+
+for (const packet of json) {
+  // environment packet
+  if (packet.env) {
+    Map.contextData.environment = packet.env;
+  }
+  // game object
+  else if (packet.id) {
+    const obj = {
+      type: packet.p.type,
+      x: packet.p.x,
+      y: packet.p.y,
+      g: packet.g,
+      p: packet.p,
+    };
+    if (packet.at) obj.at = packet.at;
+    delete obj.p.x;
+    delete obj.p.y;
+    delete obj.p.r;
+    obj.p.sh?.forEach((sh) => {
+      delete sh.o;
+      return;
+      // scale all polygons up by 20x for some reason
+      sh.v?.forEach((v) => {
+        v.x *= 20;
+        v.y *= 20;
+      });
+    });
+    Map.contextData.go.push(obj);
+  }
+}
+
+fs.writeFileSync("out.json", JSON.stringify(Map));
