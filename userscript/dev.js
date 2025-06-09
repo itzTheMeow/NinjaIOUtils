332939686c23708e65fb5df66bf4283823d90f8f
Meow <github@xela.codes>
2025-06-09 19:09:44 -0400

    v3.8

--- userscript/dev.js
+++ userscript/dev.js
@@ -16,7 +16,7 @@
 // @homepageURL  https://utils.xela.codes
 // @supportURL   https://github.com/itzTheMeow/NinjaIOUtils/issues
 // @grant        none
-// @version      3.7
+// @version      3.8
 // ==/UserScript==
 
 /*
